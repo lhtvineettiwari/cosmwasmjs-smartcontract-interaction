70214e088c23066f19fa0f4e6c92250b05784427
VineetCodes <vineet@leewayhertz.com>
2022-12-31 21:33:25 +0530

    websocket error fix

--- src/components/ChainInfo.tsx
+++ src/components/ChainInfo.tsx
@@ -1,17 +1,30 @@
-// eslint-disable-next-line
 import React, { useState, useEffect } from 'react';
 import { CheckBalance } from './CheckBalance';
+
 interface FormValues {
   rpcEndpoint: string;
   contractAddress: string;
 }
+
 export const ChainInfo: React.FC = () => {
   const [formValues, setFormValues] = useState<FormValues>({
     rpcEndpoint: '',
     contractAddress: ''
   });
-  const [showForm, setShowForm] = useState(false); // added this state variable to toggle the form visibility
-  const [showNetForm, setShowNetForm] = useState(true);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    const storedRpcEndpoint = sessionStorage.getItem("rpcEndPoint");
+    const storedContractAddress = sessionStorage.getItem("contractAddress");
+    if (storedRpcEndpoint && storedContractAddress) {
+      setFormValues({
+        rpcEndpoint: storedRpcEndpoint,
+        contractAddress: storedContractAddress
+      });
+      setShowForm(true);
+    }
+  }, []);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormValues({ ...formValues, [name]: value });
@@ -19,33 +32,21 @@ export const ChainInfo: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-       // Store state in session storage when the component mounts or the state changes
-      setShowForm(true);
-     // set showForm to false to hide the form
-     sessionStorage.setItem("rpcEndPoint", formValues.rpcEndpoint);
-     sessionStorage.setItem("contractAddress", formValues.contractAddress); 
-     window.location.reload();
+    sessionStorage.setItem("rpcEndPoint", formValues.rpcEndpoint);
+    sessionStorage.setItem("contractAddress", formValues.contractAddress);
+    setShowForm(true);
   };
-  useEffect(() => {
-   // include formValues.rpcEndpoint in the dependency array
-       if (sessionStorage.getItem("rpcEndPoint") && sessionStorage.getItem("contractAddress")){
-        setShowNetForm(false);
-        setShowForm(true);
-      }
-  }, []);
+
   const networkInfo = {
     rpc: formValues.rpcEndpoint,
     contract: formValues.contractAddress,
   }
-  
 
-  console.log("sessionStorage : ",sessionStorage.getItem("state"));
-  console.log("showForm", showForm);
-  
-  
   return (
     <>
-      {showNetForm && (
+      {showForm ? (
+        <CheckBalance netInfo={networkInfo} />
+      ) : (
         <form onSubmit={handleSubmit}>
           <label>
             RPC Endpoint:
@@ -73,7 +74,6 @@ export const ChainInfo: React.FC = () => {
           <button type="submit">Submit</button>
         </form>
       )}
-      {showForm && <CheckBalance netInfo={networkInfo} />}
     </>
   );
 };
